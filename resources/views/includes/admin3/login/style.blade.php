60410d0767163c9e07d56e1aaf3da41b12a03a0b
rikibuldan <rikibuldanalhariri@gmail.com>
2025-07-01 20:13:31 +0700

    logo

--- resources/views/includes/admin3/login/style.blade.php
+++ resources/views/includes/admin3/login/style.blade.php
@@ -8,8 +8,8 @@
           content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
       <meta name="author" content="pixelstrap">
       <meta name="csrf-token" content="{{ csrf_token() }}">
-      <link rel="icon" href="{{ asset('template/admin2/assets/images/favicon.png') }}" type="image/x-icon">
-      <link rel="shortcut icon" href="{{ asset('template/admin2/assets/images/favicon.png') }}" type="image/x-icon">
+      {{-- <link rel="icon" href="{{ asset('template/admin2/assets/images/favicon.png') }}" type="image/x-icon"> --}}
+      <link rel="shortcut icon" href="{{ asset('template/admin3/dist/assets/images/favicon.ico') }}">
       <title>SEWA PESTA</title>
       <!-- Google font-->
       <link rel="shortcut icon" href="{{ asset('template/admin3/dist/assets/images/favicon.ico') }}">
