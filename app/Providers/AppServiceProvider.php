2f92c7e2bffb904ee9becf1ab8d8e686d01380fb
rikibuldan <rikibuldanalhariri@gmail.com>
2025-07-12 14:40:02 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'production') {
             \URL::forceScheme('https');
         }
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \URL::forceScheme('https');
+        }
 
     }
 }
