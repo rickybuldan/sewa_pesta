1c9e4100521e484d342b05abe63449d4a9f01b26
rikibuldan <rikibuldanalhariri@gmail.com>
2025-07-27 14:41:38 +0700

    title

--- resources/views/includes/layout_frontend_two/style.blade.php
+++ resources/views/includes/layout_frontend_two/style.blade.php
@@ -1,6 +1,5 @@
 <head>
     <meta charset="utf-8">
-    <title></title>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
